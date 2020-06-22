3b30201b5852f6a96654480b8d77f38cebe7d377
EmersonOliveiraDev <emersonhaw@gmail.com>
2020-06-22 03:04:56 -0300

    Add email tests

--- __tests__/integration/user.test.js
+++ __tests__/integration/user.test.js
@@ -1,7 +1,13 @@
 import request from 'supertest';
 import app from '../../src/app';
 
+import truncate from '../util/truncate';
+
 describe('User', () => {
+  beforeEach(async () => {
+    truncate();
+  });
+
   it('Deve ser possível se cadastrar', async () => {
     const response = await request(app)
       .post('/users')
@@ -12,5 +18,25 @@ describe('User', () => {
       });
 
     expect(response.body).toHaveProperty('id');
+  });
+
+  it('O usuário não deve poder se registrar na aplicação com um e-mail duplicado', async () => {
+    await request(app)
+      .post('/users')
+      .send({
+        name: 'Emerson',
+        email: 'emersonhaw@gmail.com',
+        password_hash: '123456',
+      });
+
+    const response = await request(app)
+      .post('/users')
+      .send({
+        name: 'Emerson',
+        email: 'emersonhaw@gmail.com',
+        password_hash: '123456',
+      });
+
+    expect(response.status).toBe(400);
   });
 });
--- src/app/controllers/UserContoller.js
+++ src/app/controllers/UserContoller.js
@@ -2,8 +2,12 @@ import User from '../models/User';
 
 class UserController {
   async store(req, res) {
-    if (req.body.email === 'emersonhaw@hotmail.com') {
-      return res.status(400).json({ error: 'E-mail ruim' });
+    const { email } = req.body;
+
+    const checkEmail = await User.findOne({ where: { email } });
+
+    if (checkEmail) {
+      return res.status(400).json({ error: 'E-mail duplicado' });
     }
 
     const user = await User.create(req.body);
