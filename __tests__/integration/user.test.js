0114e19e1a863addf798a3e94cf24259b167dba2
EmersonOliveiraDev <emersonhaw@gmail.com>
2020-06-23 01:15:10 -0300

    Add generator of fake data to User

--- __tests__/integration/user.test.js
+++ __tests__/integration/user.test.js
@@ -2,7 +2,8 @@ import request from 'supertest';
 import bcrypt from 'bcryptjs';
 import app from '../../src/app';
 
-import User from '../../src/app/models/User';
+// import User from '../../src/app/models/User';
+import factory from '../factories';
 import truncate from '../util/truncate';
 
 describe('User', () => {
@@ -10,7 +11,7 @@ describe('User', () => {
     truncate();
   });
 
-  it('A senha do usuário deve ser encriptada', async () => {
+  /* it('A senha do usuário deve ser encriptada', async () => {
     const user = await User.create({
       name: 'Emerson',
       email: 'emersonhaw@gmail.com',
@@ -20,21 +21,29 @@ describe('User', () => {
     const compareHash = await bcrypt.compare('123456', user.password_hash);
 
     expect(compareHash).toBe(true);
+  }); */
+
+  it('A senha do usuário deve ser encriptada', async () => {
+    const user = await factory.create('User', {
+      password: '123456',
+    });
+
+    const compareHash = await bcrypt.compare('123456', user.password_hash);
+
+    expect(compareHash).toBe(true);
   });
 
   it('Deve ser possível se cadastrar', async () => {
+    const user = await factory.attrs('User');
+
     const response = await request(app)
       .post('/users')
-      .send({
-        name: 'Emerson',
-        email: 'emersonhaw@gmail.com',
-        password: '123456',
-      });
+      .send(user);
 
     expect(response.body).toHaveProperty('id');
   });
 
-  it('O usuário não deve poder se registrar na aplicação com um e-mail duplicado', async () => {
+  /* it('O usuário não deve poder se registrar na aplicação com um e-mail duplicado', async () => {
     await request(app)
       .post('/users')
       .send({
@@ -50,6 +59,20 @@ describe('User', () => {
         email: 'emersonhaw@gmail.com',
         password: '123456',
       });
+
+    expect(response.status).toBe(400);
+  }); */
+
+  it('O usuário não deve poder se registrar na aplicação com um e-mail duplicado', async () => {
+    const user = await factory.attrs('User');
+
+    await request(app)
+      .post('/users')
+      .send(user);
+
+    const response = await request(app)
+      .post('/users')
+      .send(user);
 
     expect(response.status).toBe(400);
   });
